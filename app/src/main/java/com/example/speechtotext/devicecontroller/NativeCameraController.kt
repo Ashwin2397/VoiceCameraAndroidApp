57a1e0710d13accabd082309f9478443b8a55348
ashwin97 <ashwix1997@gmail.com>
2021-07-20 20:36:14 -0700

    Base class ready for development.

--- app/src/main/java/com/example/speechtotext/devicecontroller/NativeCameraController.kt
+++ app/src/main/java/com/example/speechtotext/devicecontroller/NativeCameraController.kt
@@ -1,50 +1,106 @@
 package com.example.speechtotext.devicecontroller
 
-import android.content.Context
-import android.content.pm.PackageManager
-import android.hardware.Camera
-import android.hardware.camera2
-import android.hardware.camera2.CameraManager
+import android.graphics.Bitmap
+import com.example.speechtotext.ConnectionType
+import com.example.speechtotext.DeviceName
+import com.example.speechtotext.Feature
 
-class NativeCameraController(
-    val context: Context
-){
+//
+//import android.content.Context
+//import android.content.pm.PackageManager
+//import android.hardware.Camera
+//import android.hardware.camera2
+//import android.hardware.camera2.CameraManager
+//
+//    class NativeCameraController(
+//        val context: Context
+//    ){
+//
+////        val cameraManager = CameraManager
+//
+//        fun connect() {
+//
+//
+//        }
+//
+//        /*
+//        * Checks if device has a camera
+//        * @return Boolean to indicate existence of camera
+//        * */
+//        private fun checkCameraHardware(): Boolean {
+//
+//            var hasCamera = false
+//
+//            if (this.context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+//
+//                // this device has a camera
+//                hasCamera = true
+//            }
+//
+//            return hasCamera
+//        }
+//
+//        /** A safe way to get an instance of the Camera object. */
+//        private fun getCameraInstance(): Camera? {
+//            return try {
+//                Camera.open() // attempt to get a Camera instance
+//            } catch (e: Exception) {
+//                // Camera is not available (in use or does not exist)
+//                null // returns null if camera is unavailable
+//            }
+//
+//
+//        }
+//
+//
+//    }
 
-    val cameraManager = CameraManager
+class NativeCameraController: Device, Camera {
 
-    fun connect() {
-
-        cameraManager.
+    val featuresAvailable = arrayListOf<Feature>(Feature.SHOOT, Feature.ZOOM)
+    private val connectionType = ConnectionType.NATIVE
+    
+    override fun getDeviceName(): DeviceName {
+        TODO("Not yet implemented")
     }
 
-    /*
-    * Checks if device has a camera
-    * @return Boolean to indicate existence of camera
-    * */
-    private fun checkCameraHardware(): Boolean {
-
-        var hasCamera = false
-
-        if (this.context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-
-            // this device has a camera
-            hasCamera = true
-        }
-
-        return hasCamera
+    override fun connectDevice() {
+        TODO("Not yet implemented")
     }
 
-    /** A safe way to get an instance of the Camera object. */
-    private fun getCameraInstance(): Camera? {
-        return try {
-            Camera.open() // attempt to get a Camera instance
-        } catch (e: Exception) {
-            // Camera is not available (in use or does not exist)
-            null // returns null if camera is unavailable
-        }
-
-        
+    override fun getConnectionType(): ConnectionType {
+        return this.connectionType
     }
 
+    override fun isFeatureAvailable(feature: Feature): Boolean {
+        TODO("Not yet implemented")
+    }
 
+    override fun getAvailableFeatures(): ArrayList<Feature> {
+        return featuresAvailable
+    }
+
+    override fun configureLiveview(callback: (camera: Camera) -> Unit) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLiveviewFlip(callback: (bitmap: Bitmap?) -> Unit) {
+        TODO("Not yet implemented")
+    }
+
+    override fun shoot() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setZoom(zoomFactor: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setAperture(apertureFactor: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setFocusType(focusType: String) {
+        TODO("Not yet implemented")
+    }
 }
