ee69fffdb216f2ed4dfd2db14096f33ca5fe057b
ashwin97 <ashwix1997@gmail.com>
2021-07-28 15:01:27 -0700

    Add master controller for camera and gimbal.

---
+++ app/src/main/java/com/example/speechtotext/devicecontroller/MasterControllers.kt
@@ -0,0 +1,45 @@
+package com.example.speechtotext.devicecontroller
+
+import android.graphics.Bitmap
+import com.example.speechtotext.DeviceName
+import com.example.speechtotext.Word
+
+object MasterCamera {
+
+    val factory = MasterControllerFactory()
+    var chosenCamera = DeviceName.CANON
+
+    fun shoot(word: Word) {
+
+        factory.getCameraInstance(chosenCamera).shoot()
+    }
+
+    fun setZoom(word: Word) {
+
+        // Change parameter for setZoom to a string
+        factory.getCameraInstance(chosenCamera).setZoom(word.value)
+    }
+
+    fun setAperture(word: Word) {
+
+        // Change parameter for setAperture to a float
+        factory.getCameraInstance(chosenCamera).setAperture(word.value)
+    }
+
+    fun setFocusType(word: Word) {
+
+        factory.getCameraInstance(chosenCamera).setFocusType(word.value)
+    }
+}
+
+object MasterGimbal {
+
+    val factory = MasterControllerFactory()
+    var chosenGimbal = DeviceName.DJI_RS_2
+
+    fun move(word: Word) {
+
+        // Implement logic later on ...
+        factory.getGimbalInstance(chosenGimbal).move("","", "", 1)
+    }
+}
