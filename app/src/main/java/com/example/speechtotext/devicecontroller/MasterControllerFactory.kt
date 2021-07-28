9ae940966f5b0290d3ad6327aa7c46ea736f1c94
ashwin97 <ashwix1997@gmail.com>
2021-07-28 12:17:30 -0700

    Implement factory design pattern

---
+++ app/src/main/java/com/example/speechtotext/devicecontroller/MasterControllerFactory.kt
@@ -0,0 +1,30 @@
+package com.example.speechtotext.devicecontroller
+
+import com.example.speechtotext.DeviceName
+
+class MasterControllerFactory {
+
+    val controllers = mapOf<DeviceName, Any>(
+        DeviceName.CANON to CanonCameraController,
+        DeviceName.NATIVE to NativeCameraController,
+        DeviceName.DJI_RS_2 to DJIGimbalController,
+        DeviceName.PILOTFLY to PilotflyGimbalController,
+        DeviceName.NO_OP_CAMERA to NoOpCameraController,
+        DeviceName.NO_OP_GIMBAL to NoOpGimbalController
+    )
+
+    fun getCameraInstance(deviceName: DeviceName): Camera {
+
+        return controllers.get(deviceName) as Camera
+    }
+
+    fun getGimbalInstance(deviceName: DeviceName): Gimbal {
+
+        return controllers.get(deviceName) as Gimbal
+    }
+
+    fun getDevice(deviceName: DeviceName): Device {
+
+        return controllers.get(deviceName) as Device
+    }
+}
