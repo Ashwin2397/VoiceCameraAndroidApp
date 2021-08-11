aaa266d77b14fa5dfe90493cbc692ccd1f1049ea
ashwin97 <ashwix1997@gmail.com>
2021-08-10 17:23:44 -0700

    Set up connectDevice() function. Add BT capabilities.

--- app/src/main/java/com/example/speechtotext/devicecontroller/PilotflyGimbalController.kt
+++ app/src/main/java/com/example/speechtotext/devicecontroller/PilotflyGimbalController.kt
@@ -1,27 +1,106 @@
 package com.example.speechtotext.devicecontroller
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Context
-import com.example.speechtotext.ConnectionType
-import com.example.speechtotext.DeviceName
-import com.example.speechtotext.Feature
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.speechtotext.*
 import java.lang.ref.WeakReference
 
 object PilotflyGimbalController: Device, Gimbal {
 
-    val featuresAvailable = arrayListOf<Feature>(Feature.ABSOLUTE_MOVEMENT, Feature.INCREMENTAL_MOVEMENT)
+//    val featuresAvailable = arrayListOf<Feature>(Feature.ABSOLUTE_MOVEMENT, Feature.INCREMENTAL_MOVEMENT)
+    private val featuresAvailable = arrayListOf<Feature>(Feature.LEFT, Feature.RIGHT, Feature.UP, Feature.DOWN, Feature.ROLL)
+
+
     private val connectionType = ConnectionType.BLUETOOTH
-    private val deviceName = DeviceName.PILOTFLY
+    private val DEVICE_NAME = DeviceName.PILOTFLY
     lateinit var context: WeakReference<Context>
+
+    // BT variables
+    lateinit var bluetoothAdapter: BluetoothAdapter
+    val REQUEST_ENABLE_BT = 0
+    private val SCAN_PERIOD: Long = 10000 // Stops scanning after 10 seconds.
+    val bleService = BluetoothLeService()
+
 
     override fun setApplicationContext(context: Context) {
         this.context = WeakReference(context)
     }
 
     override fun getDeviceName(): DeviceName {
-        return this.deviceName
+        return this.DEVICE_NAME
     }
 
-    override fun connectDevice() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun connectDevice(mainActivity: MainActivity) {
+
+        val permissions: Array<String> = arrayOf(android.Manifest.permission.BLUETOOTH, android.Manifest.permission.ACCESS_FINE_LOCATION)
+        mainActivity.requestPermissions( permissions, 101)
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            mainActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }else {
+
+            scanLeDevice()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun scanLeDevice() {
+
+        val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+        var scanning = false
+        val handler = Handler()
+
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            scanning = true
+            bluetoothLeScanner.startScan(
+                listOf<ScanFilter>(ScanFilter.Builder().setDeviceName("Pilotfly").build()),
+                ScanSettings.Builder().build(),
+                leScanCallback,
+            )
+        } else {
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+
+    private val leScanCallback: ScanCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    object : ScanCallback() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+
+            Log.d("BLE_SCANNER", "Device: ${result.device.name}")
+//            device = result.device
+//
+            // Connect to device
+            bleService.initialize()
+            bleService.connect(result.device.address)
+
+            // Pass service to Pilotfly controller to use
+
+        }
     }
 
     override fun getConnectionType(): ConnectionType {
@@ -44,5 +123,7 @@ object PilotflyGimbalController: Device, Gimbal {
     }
 
     override fun move(yaw: String, pitch: String, roll: String, isAbsolute: Int) {
+
+        bleService.sendCommand()
     }
 }
